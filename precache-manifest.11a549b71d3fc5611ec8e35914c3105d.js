296171e46ddd42c1b807ad5cd3fa4d16ad81843f
James <james.turner987@outlook.com>
2025-04-22 11:11:54 +0100

    Updates

---
+++ precache-manifest.11a549b71d3fc5611ec8e35914c3105d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd6032530129b1fbd0e8c1e39f47c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c518fc869a7b499b87",
+    "url": "/static/css/main.d7ba75d4.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "e6c518fc869a7b499b87",
+    "url": "/static/js/main.82dfd594.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
